641e91e7794410f404d83c836136214085ead9f8
刘启明 <455043818@qq.com>
2019-09-24 15:47:38 +0800

    显示成绩

--- 2019-09-23-数字字母小游戏/alphanumeric.py
+++ 2019-09-23-数字字母小游戏/alphanumeric.py
@@ -16,6 +16,7 @@ keyBoardData={48:'0',49:'1',50:'2',51:'3',52:'4',53:'5',54:'6',55:'7',56:'8',57:
                 97:'a',98:'b',99:'c',100:'d',101:'e',102:'f',103:'g',104:'h',105:'i',106:'j',
                 107:'k',108:'l',109:'m',110:'n',111:'o',112:'p',113:'q',114:'r',115:'s',116:'t',
                 117:'u',118:'v',119:'w',120:'x',121:'y',122:'z'}
+
 # 颜色定义
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -26,6 +27,11 @@ Green = (0, 255, 0)
 Red = (255, 0, 0)
 blue = (0, 0, 255)
 dark_blue =(0,0, 139)
+
+successTimes=0
+perSuccessScore=5
+failTimes=0
+perFailScore=5
 
 #游戏背景颜色
 BG_COLOR = black 
@@ -60,7 +66,6 @@ def show_start_info(screen):
                 else:
                     return     #结束此函数
 
-
 #游戏主体运行
 def running_game(screen,clocker):
     coords=[]
@@ -80,13 +85,27 @@ def running_game(screen,clocker):
                 if(event.key ==K_ESCAPE):
                     terminate()  #终止程序
                 else:
+                    global successTimes
+                    global failTimes
                     #判断按键是否与显示值匹配，并计算成绩
-                    value= keyBoardData[event.key]
-                    for coord in coords:
-                        if(coord['value'] ==value):
-                            coords.remove(coord)
-                            break
-        clocker.tick(1) #控制fps
+                    index=0
+                    delBool=False
+                    if(event.key>=48 and event.key<=57) or (event.key>=97 and event.key<=122):
+                        value= keyBoardData[event.key]
+                        for coord in coords:
+                            if(coord['value'] ==value):
+                                coords.remove(coord)
+                                successTimes+=1
+                                delBool=True
+                                #show_result_info(screen,"+")   #匹配加分
+                                break
+                        if(delBool==False):
+                            failTimes+=1
+
+                    else:
+                        failTimes+=1
+                    #show_result_info(screen,"-")   #没有匹配减分
+        clocker.tick(1) #每秒钟一次刷新
         newCoords=[]
         #数据下移
         for coord in coords:
@@ -95,10 +114,30 @@ def running_game(screen,clocker):
             scoreSurf = font.render('%s' % coord['value'], True, Green)
             scoreRect = scoreSurf.get_rect()
             scoreRect.topleft = (coord['x'], coord['y'])
-            screen.blit(scoreSurf, scoreRect)
-                    
+            screen.blit(scoreSurf, scoreRect)    
+
+        show_result_info(screen)            
         pygame.display.update()
-    
+
+#成绩输出
+def show_result_info(screen):
+    score=perSuccessScore*successTimes-perFailScore*failTimes
+    font = pygame.font.Font('myfont.ttf', 20)
+    scoreSurf = font.render('总分数%s' %score, True, Red)
+    scoreRect = scoreSurf.get_rect()
+    scoreRect.topleft = (702, 50)
+    screen.blit(scoreSurf, scoreRect)
+
+    succeSurf=font.render('成功次数：%s' %successTimes,True,Red)
+    succeRect=succeSurf.get_rect()
+    succeRect.topleft = (702, 80)
+    screen.blit(succeSurf, succeRect)    
+
+    failSurf=font.render('失败次数：%s' %failTimes,True,Red)
+    failRect=succeSurf.get_rect()
+    failRect.topleft = (702, 120)
+    screen.blit(failSurf, failRect)  
+
 #程序终止
 def terminate():
     pygame.quit()
